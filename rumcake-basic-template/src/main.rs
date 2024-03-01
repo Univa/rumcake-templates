6da03d44341fa8c6652974663eb3d81ab897873e
Univa <41708691+Univa@users.noreply.github.com>
2024-03-01 11:32:16 -0500

    fix remap_matrix call

--- rumcake-basic-template/src/main.rs
+++ rumcake-basic-template/src/main.rs
@@ -25,15 +25,15 @@ pub struct {{ keyboard-name }};
 remap_matrix! {
     {
         [ K00 K01 K02 K03 K04 K05 K06 K07 ]
-        [ K08 K09 K10 K11 K12 K13 K14 #No ]
+        [ K08 K09 K10 K11 K12 K13 K14 No  ]
         [ K15 K16 K17 K18 K19 K20 K21 K22 ]
-        [ K23 K24 K25 K26 K27 K28 K29 #No ]
+        [ K23 K24 K25 K26 K27 K28 K29 No  ]
         [ K30 K31 K32 K33 K34 K35 K36 K37 ]
-        [ K38 K39 K40 K41 K42 K43 K44 #No ]
+        [ K38 K39 K40 K41 K42 K43 K44 No  ]
         [ K45 K46 K47 K48 K49 K50 K51 K52 ]
-        [ K53 K54 K55 K56 K57 K58 K59 #No ]
+        [ K53 K54 K55 K56 K57 K58 K59 No  ]
         [ K60 K61 K62 K63 K64 K65 K66 K67 ]
-        [ #No #No #No #No #No K68 K69 #No ]
+        [ No  No  No  No  No  K68 K69 No  ]
     }
     {
         [ K00 K08 K01 K09 K02 K10 K03 K11 K04 K12 K05 K13 K06 K14 K07 K22 ]
